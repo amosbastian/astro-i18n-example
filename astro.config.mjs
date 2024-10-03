5228c4a2bc3f5e3020c4f97d0e91b1d7874f748e
Amos Bastian <amosbastian@gmail.com>
2024-10-03 21:08:14 +0200

    chore: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import liciousI18n from '@astrolicious/i18n';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [liciousI18n({
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+  })]
+});
